ebe2a7161d44ca010f2451a0a02b96a0398cecd0
dunow <dunow33@hotmail.com>
2020-09-15 11:44:59 -0400

    Doing some work in the apiGrab file

--- lib/movie_times/apiGrab.rb
+++ lib/movie_times/apiGrab.rb
@@ -1,3 +1,20 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'open-uri'
+require 'json'
+
 class API
-    
+    API_key = '943027A1-B7B7-4A74-81C4-C3C760B6F7AE'
+
+    url = ("http://www.omdbapi.com/?apikey=e6fd8697&t=The%20Matrix")
+    uri = URI.parse(url)
+
+    response = Net::HTTP.get_response(uri)
+
+    puts JSON.parse(response.read_body)
+    # movies = JSON.parse(response.read_body)
+    # movies.each do | movie |
+    #     puts movie["Title"]
+    # end
 end
--- lib/movie_times/movies.rb
+++ lib/movie_times/movies.rb
@@ -3,19 +3,20 @@ class Movies
 
     @@movies = []
 
+    def self.movie_loop
+        #I think this method will really be used to get info from the API class
+        the_matrix = self.new("The Matrix", "Andy and Larry Wachowski", 1999, "R")
+        gladiator = self.new("Gladiator", "Ridley Scott", 2000, "R")
+        the_departed = self.new("The Departed", "Martin Scorsese", 2006, "R")
+        limitless = self.new("Limitless", "Neil Berger", 2011, "PG-13")
+    end
+
     def initialize(title, director, year_released, rating)
         @title = title
         @director = director
         @year_released = year_released
         @rating = rating
         @@movies << self
-    end
-
-    def self.movie_loop
-        the_matrix = self.new("The Matrix", "Andy and Larry Wachowski", 1999, "R")
-        gladiator = self.new("Gladiator", "Ridley Scott", 2000, "R")
-        the_departed = self.new("The Departed", "Martin Scorsese", 2006, "R")
-        limitless = self.new("Limitless", "Neil Berger", 2011, "PG-13")
     end
 
     def self.all
