f65fde1f8fc62f4493c9810dc3b5bcd1f1c56ff3
dunow <dunow33@hotmail.com>
2020-09-19 14:59:24 -0400

    program logic finished, finished adding comments to cli and movies classes

--- lib/movie_times/cli.rb
+++ lib/movie_times/cli.rb
@@ -39,25 +39,38 @@ class MovieTimes::CLI
 
             #ask the user if they would like to see some additional trivia for the movie that they entered
             print "Would you like to see additonal info for that movie or see trivia for another movie? ('y' or 'n'):  "
+
+            #function to get the answer about whether or not they want to see additional movie trivia
             additional_movie_answer
+
+            #function to move on to next movie after additional movie trivia displayed
             movie = next_movie
         end
         puts
+
+        #exit message
         puts "Thank you!!  Have a good day!!"
         puts
+
+        #exit the program
         exit
     end
 
+    #function to get the user input
     def prompt_for_answer
         gets.chomp
     end
 
+    #function to print message asking for next movie title, then getting user input
     def next_movie
         puts
         print "Please enter another movie title (or 'q' to quit):  "
         prompt_for_answer
     end
 
+    #function to display initial set of movie trivia...it does this by calling movie class
+    #and the function in that class to retieve the last movie entered, and by called on
+    #attributes of that movie
     def display_movie_data
         puts
         puts "Movie: " + MovieTimes::Movies.last.title.to_s
@@ -68,21 +81,26 @@ class MovieTimes::CLI
         puts
     end
 
+    #function that uses an if statement to get user's input on whether or not to display additional movie trivia
     def additional_movie_answer
         answer = prompt_for_answer
         if answer == 'y'
+            #if the user wants to see additional data, call that particular function
             additional_movie_data
         elsif answer == 'n'
             puts
             puts
+            #if the user does not want to see additional data, call the original loop asking for input
             greet_user
         else
             puts "Sorry, I didn't understand that response!!  Let's go back to the beginning!!"
             puts
+            #if the user enters an invalid response, take them back to the original loop
             greet_user
         end
     end
 
+    #function to display additional movie trivia
     def additional_movie_data
         puts
         puts "Writer: " + MovieTimes::Movies.last.writer.to_s
--- lib/movie_times/movies.rb
+++ lib/movie_times/movies.rb
@@ -1,8 +1,15 @@
 class MovieTimes::Movies
+
+    #attribute accessors for every movie attribute
     attr_accessor :title, :director, :writer, :actors, :plot, :date_released, :rating, :awards
 
+    #array to store every movie entered by the user.  if this app were to get fleshed out more,
+    #i would have liked the ability to ask the user if they wanted to display information about any
+    #of the movies they had already entered.  storing the movies as they are entered would make that
+    #possible.
     @@movies = []
 
+    #intializes each movie as it's entered with the 8 characteristics that the API class is retrieving
     def initialize(title, director, writer, actors, plot, date_released, rating, awards)
         @title = title
         @director = director
@@ -15,10 +22,12 @@ class MovieTimes::Movies
         @@movies << self
     end
 
+    #returns the entire array for potential future functionality
     def self.all
         @@movies
     end
 
+    #retrieves the last movie entered, which is necessary to get the last movie's trivia from the array
     def self.last
         @@movies.last
     end
